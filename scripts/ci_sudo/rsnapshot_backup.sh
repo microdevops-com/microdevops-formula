014f424d165ce01d6cd4ce08b8038e9dd53c0bf2
Eugene Antonov <jetman@sysadm.ws>
2018-08-14 18:22:33 +0300

    shades of red added

--- scripts/ci_sudo/rsnapshot_backup.sh
+++ scripts/ci_sudo/rsnapshot_backup.sh
@@ -23,6 +23,8 @@ stdbuf -oL -eL echo '---'
 stdbuf -oL -eL echo 'CMD: salt --force-color -t 43200 -C "I@rsnapshot_backup:backup_server:True'${MOD}'" cmd.run "/opt/sysadmws/rsnapshot_backup/rsnapshot_backup_sync_monthly_weekly_daily_check_backup.sh"'
 ( stdbuf -oL -eL salt --force-color -t 43200 -C "I@rsnapshot_backup:backup_server:True${MOD}" cmd.run "/opt/sysadmws/rsnapshot_backup/rsnapshot_backup_sync_monthly_weekly_daily_check_backup.sh" || GRAND_EXIT=1 ) | ccze -A | sed -e 's/33mNOTICE/32mNOTICE/'
 
+# Shades of red
 grep -q "\[0;31m" /srv/scripts/ci_sudo/$(basename $0).out && GRAND_EXIT=1
+grep -q "\[31m" /srv/scripts/ci_sudo/$(basename $0).out && GRAND_EXIT=1
 
 exit $GRAND_EXIT
--- scripts/ci_sudo/test_ping.sh
+++ scripts/ci_sudo/test_ping.sh
@@ -8,6 +8,8 @@ stdbuf -oL -eL echo "---"
 stdbuf -oL -eL echo "NOTICE: CMD: salt --force-color -t 300 '*' test.ping"
 stdbuf -oL -eL salt --force-color -t 300 '*' test.ping
 
+# Shades of red
 grep -q "\[0;31m" /srv/scripts/ci_sudo/$(basename $0).out && GRAND_EXIT=1
+grep -q "\[31m" /srv/scripts/ci_sudo/$(basename $0).out && GRAND_EXIT=1
 
 exit $GRAND_EXIT
