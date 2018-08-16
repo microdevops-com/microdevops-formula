bdf093f23063f6a9885d1a49d71dac3cbbb7a43f
Eugene Antonov <jetman@sysadm.ws>
2018-08-16 16:15:11 +0300

    ci out for diff arg 1

--- scripts/ci_sudo/rsnapshot_backup.sh
+++ scripts/ci_sudo/rsnapshot_backup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 GRAND_EXIT=0
-rm -f /srv/scripts/ci_sudo/$(basename $0).out
-exec > >(tee /srv/scripts/ci_sudo/$(basename $0).out)
-exec 2>&1
 
 if [ "_$1" != "_" ]; then
 	MOD=" and $1"
+	MOD_SHA=$(echo "$1" | sha1sum | awk '{print $1}')
+	OUT_FILE="/srv/scripts/ci_sudo/$(basename $0)_${MOD_SHA}.out"
 else
 	MOD=""
+	OUT_FILE="/srv/scripts/ci_sudo/$(basename $0).out"
 fi
+
+rm -f ${OUT_FILE}
+exec > >(tee ${OUT_FILE})
+exec 2>&1
 
 stdbuf -oL -eL echo '---'
 stdbuf -oL -eL echo 'CMD: salt --force-color -t 300 -C "I@rsnapshot_backup:*'${MOD}'" state.apply --state-output=filter --state-verbose=False exclude=True, rsnapshot_backup.put_check_files'
@@ -23,8 +27,8 @@ stdbuf -oL -eL echo '---'
 stdbuf -oL -eL echo 'CMD: salt --force-color -t 43200 -C "I@rsnapshot_backup:backup_server:True'${MOD}'" cmd.run "/opt/sysadmws/rsnapshot_backup/rsnapshot_backup_sync_monthly_weekly_daily_check_backup.sh"'
 ( stdbuf -oL -eL salt --force-color -t 43200 -C "I@rsnapshot_backup:backup_server:True${MOD}" cmd.run "/opt/sysadmws/rsnapshot_backup/rsnapshot_backup_sync_monthly_weekly_daily_check_backup.sh" || GRAND_EXIT=1 ) | ccze -A | sed -e 's/33mNOTICE/32mNOTICE/'
 
-# Shades of red
-grep -q "\[0;31m" /srv/scripts/ci_sudo/$(basename $0).out && GRAND_EXIT=1
-grep -q "\[31m" /srv/scripts/ci_sudo/$(basename $0).out && GRAND_EXIT=1
+# 50 shades of red
+grep -q "\[0;31m" ${OUT_FILE} && GRAND_EXIT=1
+grep -q "\[31m" ${OUT_FILE} && GRAND_EXIT=1
 
 exit $GRAND_EXIT
