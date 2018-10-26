a396ef492c833decd9f4fde307fd34f660f2f8ba
Eugene Antonov <jetman@sysadm.ws>
2018-10-26 16:36:47 +0300

    jessie lxd bootstrap fix

--- lxd/scripts/bootstrap_jessie_network.sh
+++ lxd/scripts/bootstrap_jessie_network.sh
@@ -9,11 +9,12 @@ iface eth0 inet static
         address $1
         netmask $2
         gateway $3
-        dns-nameservers $4
-        dns-search $5
 EOM
 
 [[ ! -z $6 ]] && echo "        hwaddress ether $6" >> /etc/network/interfaces
+
+echo "search $5" > /etc/resolv.conf
+for NS in $4; do echo "nameserver ${NS}" >> /etc/resolv.conf; done
 
 /bin/kill -9 `/bin/ps ax | /bin/grep dhclient | /bin/grep -v grep | /usr/bin/awk '{print $1}'`
 /sbin/ifdown --force eth0
