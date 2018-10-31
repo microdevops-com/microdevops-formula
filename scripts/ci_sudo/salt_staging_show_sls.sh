22fbecba5f73bab8ac090177e5521850895767f3
Eugene Antonov <jetman@sysadm.ws>
2018-10-31 18:37:22 +0200

    state list debug added

--- scripts/ci_sudo/salt_staging_show_sls.sh
+++ scripts/ci_sudo/salt_staging_show_sls.sh
@@ -68,6 +68,12 @@ else
 	GREP_E=""
 fi
 
+# Get the list of states and echo for debug
+stdbuf -oL -eL echo "NOTICE: found states:"
+for STATE in $(salt-call --local --config-dir=${WORK_DIR}/etc/salt cp.list_states | awk '{print $2}' | grep -v "^top$" ${GREP_E}); do
+	stdbuf -oL -eL echo "${STATE}"
+done
+
 # Get the list of states and render them
 for STATE in $(salt-call --local --config-dir=${WORK_DIR}/etc/salt cp.list_states | awk '{print $2}' | grep -v "^top$" ${GREP_E}); do
 	stdbuf -oL -eL echo "NOTICE: checking state ${STATE}"
