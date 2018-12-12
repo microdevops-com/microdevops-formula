7fc30a316401b927654c8fab17ee0b4aa829af6a
Eugene Antonov <jetman@sysadm.ws>
2018-12-12 11:47:12 +0200

    xenial bootstrap lxd two networks

---
+++ lxd/scripts/bootstrap_xenial_network_two_nets.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cat > /etc/network/interfaces <<- EOM
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet static
+        address $1
+        netmask $2
+        gateway $3
+        dns-nameservers $4
+        dns-search $5
+
+auto eth1
+iface eth0 inet static
+	address $6
+	netmask $7
+EOM
+
+[[ ! -z $8 ]] && echo "        hwaddress ether $6" >> /etc/network/interfaces
+
+/bin/kill -9 `/bin/ps ax | /bin/grep dhclient | /bin/grep -v grep | /usr/bin/awk '{print $1}'`
+/bin/sleep 2
+/bin/kill -9 `/bin/ps ax | /bin/grep dhclient | /bin/grep -v grep | /usr/bin/awk '{print $1}'`
+/sbin/ifdown --force eth0
+/bin/sleep 2
+/sbin/ifup eth0
+/bin/sleep 5
