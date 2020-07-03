3274bfeec7ffba95d8ea17e1eac8f3e22897a59a
Eugene Antonov <jetman@sysadm.ws>
2020-07-03 12:09:10 +0300

    lxd bootstrap focal

---
+++ lxd/bootstrap/focal_salt_minion.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+apt-get -qy -o 'DPkg::Options::=--force-confold' -o 'DPkg::Options::=--force-confdef' install wget gnupg
+echo "deb http://repo.saltstack.com/py3/ubuntu/$(lsb_release -sr)/amd64/$2 $(lsb_release -sc) main">> /etc/apt/sources.list.d/saltstack.list
+wget -qO - https://repo.saltstack.com/py3/ubuntu/$(lsb_release -sr)/amd64/$2/SALTSTACK-GPG-KEY.pub | apt-key add -
+
+[[ -d /home/ubuntu ]] && userdel -r "ubuntu"
+
+apt-get update
+apt-get -qy -o 'DPkg::Options::=--force-confold' -o 'DPkg::Options::=--force-confdef' upgrade
+apt-get -qy -o 'DPkg::Options::=--force-confold' -o 'DPkg::Options::=--force-confdef' dist-upgrade
+
+apt-get -qy -o 'DPkg::Options::=--force-confold' -o 'DPkg::Options::=--force-confdef' install salt-minion
+
+rm -f /etc/salt/minion_id
+cp /etc/hostname /etc/salt/minion_id
+
+cat > /etc/salt/minion <<- EOM
+master:
+  - $1
+grains:
+  fqdn: $(cat /etc/salt/minion_id)
+EOM
+
+service salt-minion restart
