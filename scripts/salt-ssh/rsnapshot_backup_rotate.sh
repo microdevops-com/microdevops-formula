8e8b52c03217a55dff779ad76d62d99d5fff70b7
Eugene Antonov <jetman@sysadm.ws>
2020-10-13 09:30:16 +0300

    more salt-ssh scripts

---
+++ scripts/salt-ssh/rsnapshot_backup_rotate.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+GRAND_EXIT=0
+
+if [[ "_$1" = "_" || "_$2" = "_" ]]; then
+	echo ERROR: needed args missing: use rsnapshot_backup_rotate.sh TARGET SSH/SALT SSH_HOST SSH_PORT SSH_JUMP
+	echo ERROR: SSH_HOST, SSH_PORT, SSH_JUMP - optional
+	exit 1
+fi
+
+TARGET=$1
+RSNAPSHOT_BACKUP_TYPE=$2
+
+if [ "${RSNAPSHOT_BACKUP_TYPE}" = "SSH" ]; then
+	if [ "_$5" = "_" ]; then
+		SSH_JUMP=""
+	else
+		SSH_JUMP="-J $5"
+	fi
+	if [ "_$4" = "_" ]; then
+		SSH_PORT=22
+	else
+		SSH_PORT=$4
+	fi
+	if [ "_$3" = "_" ]; then
+		SSH_HOST=${TARGET}
+	else
+		SSH_HOST=$3
+	fi
+fi
+	
+OUT_FILE="$(mktemp -p /dev/shm/)"
+
+exec > >(tee ${OUT_FILE})
+exec 2>&1
+
+if [ "${RSNAPSHOT_BACKUP_TYPE}" = "SSH" ]; then
+	# Monthly
+	( set -x ; set -o pipefail && stdbuf -oL -eL ssh -o BatchMode=yes -o StrictHostKeyChecking=no ${SSH_JUMP} -p ${SSH_PORT} ${SSH_HOST} "bash -c 'exec > >(tee /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.log); exec 2>&1; if [ $(date +%d) = 01 ]; then /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh monthly; fi'" | ccze -A | sed -e 's/33mNOTICE/32mNOTICE/' ) || GRAND_EXIT=1
+	# Weekly
+	( set -x ; set -o pipefail && stdbuf -oL -eL ssh -o BatchMode=yes -o StrictHostKeyChecking=no ${SSH_JUMP} -p ${SSH_PORT} ${SSH_HOST} "bash -c 'exec > >(tee /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.log); exec 2>&1; if [ $(date +%u) = 1 ]; then /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh weekly; fi'" | ccze -A | sed -e 's/33mNOTICE/32mNOTICE/' ) || GRAND_EXIT=1
+	# Daily
+	( set -x ; set -o pipefail && stdbuf -oL -eL ssh -o BatchMode=yes -o StrictHostKeyChecking=no ${SSH_JUMP} -p ${SSH_PORT} ${SSH_HOST} "bash -c 'exec > >(tee /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.log); exec 2>&1; /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh daily'" | ccze -A | sed -e 's/33mNOTICE/32mNOTICE/' ) || GRAND_EXIT=1
+elif [ "${RSNAPSHOT_BACKUP_TYPE}" = "SALT" ]; then
+	# Monthly
+	( set -x ; set -o pipefail && stdbuf -oL -eL salt-ssh --force-color ${TARGET} cmd.run "bash -c 'exec > >(tee /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.log); exec 2>&1; if [ $(date +%d) = 01 ]; then /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh monthly; fi'" | ccze -A | sed -e 's/33mNOTICE/32mNOTICE/' ) || GRAND_EXIT=1
+	# Weekly
+	( set -x ; set -o pipefail && stdbuf -oL -eL salt-ssh --force-color ${TARGET} cmd.run "bash -c 'exec > >(tee /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.log); exec 2>&1; if [ $(date +%u) = 1 ]; then /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh weekly; fi'" | ccze -A | sed -e 's/33mNOTICE/32mNOTICE/' ) || GRAND_EXIT=1
+	# Daily
+	( set -x ; set -o pipefail && stdbuf -oL -eL salt-ssh --force-color ${TARGET} cmd.run "bash -c 'exec > >(tee /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.log); exec 2>&1; /opt/sysadmws/rsnapshot_backup/rsnapshot_backup.sh daily'" | ccze -A | sed -e 's/33mNOTICE/32mNOTICE/' ) || GRAND_EXIT=1
+else
+	echo ERROR: unknown RSNAPSHOT_BACKUP_TYPE
+	exit 1
+fi
+
+# Check out file for errors
+grep -q "ERROR" ${OUT_FILE} && GRAND_EXIT=1
+
+# Check out file for red color with shades 
+grep -q "\[0;31m" ${OUT_FILE} && GRAND_EXIT=1
+grep -q "\[31m" ${OUT_FILE} && GRAND_EXIT=1
+grep -q "\[0;1;31m" ${OUT_FILE} && GRAND_EXIT=1
+
+exit $GRAND_EXIT
---
+++ scripts/salt-ssh/rsnapshot_backup_update_config.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+GRAND_EXIT=0
+
+if [[ "_$1" = "_" ]]; then
+	echo ERROR: needed args missing: use rsnapshot_backup_update_config.sh TARGET
+	exit 1
+fi
+
+SALT_TARGET=$1
+	
+OUT_FILE="$(mktemp -p /dev/shm/)"
+
+exec > >(tee ${OUT_FILE})
+exec 2>&1
+
+( set -x ; stdbuf -oL -eL  bash -c "salt-ssh --force-color ${SALT_TARGET} state.apply rsnapshot_backup.update_config queue=True" ) || GRAND_EXIT=1
+
+# Check out file for errors
+grep -q "ERROR" ${OUT_FILE} && GRAND_EXIT=1
+
+# Check out file for red color with shades 
+grep -q "\[0;31m" ${OUT_FILE} && GRAND_EXIT=1
+grep -q "\[31m" ${OUT_FILE} && GRAND_EXIT=1
+grep -q "\[0;1;31m" ${OUT_FILE} && GRAND_EXIT=1
+
+exit $GRAND_EXIT
