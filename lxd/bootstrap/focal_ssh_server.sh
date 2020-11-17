011d17dd7363c33f79519e7362f5a5e81a45c57e
Eugene Antonov <jetman@sysadm.ws>
2020-11-17 15:28:12 +0200

    focal lxd scripts

--- lxd/bootstrap/focal_ssh_server.sh
+++ lxd/bootstrap/focal_ssh_server.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
 apt-get -qy -o 'DPkg::Options::=--force-confold' -o 'DPkg::Options::=--force-confdef' install openssh-server
-
-mkdir -p -m 0700 /root/.ssh
-
-echo "$1" >> /root/.ssh/authorized_keys
-chmod 600 /root/.ssh/authorized_keys
