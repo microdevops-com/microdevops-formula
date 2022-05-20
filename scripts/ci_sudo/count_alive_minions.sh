09ec9147608d0dd52353ca81066279e92e7c12df
Eugene Antonov <jetman@sysadm.ws>
2022-05-20 14:37:33 +0300

    manage.up back for counting minions

--- scripts/ci_sudo/count_alive_minions.sh
+++ scripts/ci_sudo/count_alive_minions.sh
@@ -7,4 +7,7 @@ fi
 
 MINION_NAME=$1
 
-salt-run manage.alived --out=json | jq '.[]|select(. == "'${MINION_NAME}'")' | wc -l
+# manage.alived works up to 2 secs but does not show windows minions for instance
+# manage.up works sometimes up to 20 secs, but show windows minions, better to use this func with pipeline cache
+#salt-run manage.alived --out=json | jq '.[]|select(. == "'${MINION_NAME}'")' | wc -l
+salt-run manage.up --out=json | jq '.[]|select(. == "'${MINION_NAME}'")' | wc -l
