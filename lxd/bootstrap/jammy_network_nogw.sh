47a6ff8b9d795c99b87c467b3e5053edae1f2f80
eugengubich <eugengubich@gmail.com>
2023-02-14 15:38:47 +0200

    added jammy nogw bootstrap lxd

---
+++ lxd/bootstrap/jammy_network_nogw.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cat > /etc/netplan/10-lxc.yaml <<- EOM
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    eth0:
+      macaddress: $5
+      addresses:
+        - $1/$2
+      routes:
+        - on-link: true
+          to: 0.0.0.0/0
+      nameservers:
+        search: $4
+        addresses: $3
+EOM
+
+netplan apply
