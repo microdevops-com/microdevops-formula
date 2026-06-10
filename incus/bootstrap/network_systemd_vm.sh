77c25a3b001dd187f29daafc1f707d4519a92785
Viacheslav Krukovskyi <59113856+true-spirit@users.noreply.github.com>
2026-06-10 17:16:23 +0200

    refactor(network): match interfaces by MACAddress and remove DBus wait (#762)

--- incus/bootstrap/network_systemd_vm.sh
+++ incus/bootstrap/network_systemd_vm.sh
@@ -2,7 +2,7 @@
 
 cat > /etc/systemd/network/enp5s0.network <<- EOM
 [Match]
-Name=enp5s0
+MACAddress=$6
 
 [Network]
 Address=$1/$2
@@ -12,9 +12,6 @@ Domains=$5
 DHCP=no
 LinkLocalAddressing=ipv4
 EOM
-
-# Wait dbus to be available
-timeout 30s bash -c 'until [[ -n $DBUS_SESSION_BUS_ADDRESS1 ]]; do echo -n .; sleep 1; done'
 
 systemctl restart systemd-networkd
 systemctl restart systemd-resolved
